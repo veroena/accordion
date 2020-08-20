f58c90af7111b07d5b976e47eae4d0e71acdbda3
veroena <veronica.enamorado@gmail.com>
2020-08-20 18:03:22 +0200

    fetch data and paint the accordion

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,84 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
-// const ENDPOINT = 'https://debug-api.fastpayhotels.net/DataService/EntityService.svc/ServiceType?$filter=(IdServiceCatego%20ry%20eq%202)%20or%20(IdServiceCategory%20eq%203)%20or%20(IdServiceCategory%20eq%204)&$format=json&$expand=ServiceCategory&$select=Id,Free,Caption,Name,ServiceCategory/Id,Servic%20eCategory/Name,ServiceCategory/Caption';
+const ENDPOINT = 'https://debug-api.fastpayhotels.net/DataService/EntityService.svc/ServiceType?$filter=(IdServiceCategory%20eq%202)%20or%20(IdServiceCategory%20eq%203)%20or%20(IdServiceCategory%20eq%204)&$format=json&$expand=ServiceCategory&$select=Id,Free,Caption,Name,ServiceCategory/Id,ServiceCategory/Name,ServiceCategory/Caption';
 
-// const fetchData = () => {
-//   fetch(ENDPOINT)
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log(data);
-//     })
-// }
 
-const data = [
-  {"ServiceCategory": {
-    "Id": "1",
-    "Caption": "Internet",
-    "Name": "Internet"
-    },
-  "Id": "1",
-  "Free": true,
-  "Caption": "Internet",
-  "Name": "Internet",
-  "children": [
-    {"name": "1"},
-    {"name": "2"},
-    {"name": "3"},
-    {"name": "4"},
-  ]
-  },
-  {"ServiceCategory": {
-    "Id": "2",
-    "Caption": "Kids Services",
-    "Name": "Kids Services"
-    },
-  "Id": "2",
-  "Free": true,
-  "Caption": "Kids Services",
-  "Name": "Kids Services",
-  "children": [
-    {"name": "a"},
-    {"name": "b"},
-    {"name": "c"},
-    {"name": "d"},
-  ]
-  },
-  {"ServiceCategory": {
-    "Id": "3",
-    "Caption": "Services",
-    "Name": "Services"
-    },
-  "Id": "3",
-  "Free": true,
-  "Caption": "Services",
-  "Name": "Services",
-  "children": [
-    {"name": "11"},
-    {"name": "22"},
-    {"name": "33"},
-    {"name": "44"},
-  ]
-  },
-]
 
 class App extends React.Component {
-  // fetchData();
-
   constructor(props) {
     super(props);
     this.state = {
-      idOpen: 1,
+      idOpen: 0,
+      services: [],
     }
+    this.fetchData = this.fetchData.bind(this);
     this.handleAccordion = this.handleAccordion.bind(this);
   }
-
+  
   handleAccordion(event) {
     const elementValue = +event.currentTarget.id;
     if (elementValue === this.state.idOpen) {
-    this.setState({idOpen : 0});
+      this.setState({idOpen : ""});
     } else {
       this.setState({idOpen : elementValue});
     }
+  }
+            
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    fetch(ENDPOINT)
+      .then(response => response.json())
+      .then(data => {
+
+        // getting the categories name
+        const categories = data.value.map(item => item.ServiceCategory.Name );
+        const newSet = new Set(categories);
+        const filteredCategories = [...newSet];
+
+        // filter data and classify them into categories for storing them in state
+        filteredCategories.forEach(item => {
+          const services = data.value.filter(element => element.ServiceCategory.Name === item);
+          this.setState({services: [...this.state.services, services]})
+        })
+      })
   }
 
   render() {
     return (
       <div className="App">
         <ul className="accordion">
-          {data.map(item => 
-            <li className="accordion__item" key={item.Id} id={item.Id} onClick={this.handleAccordion}>
-              <h2 className="accordion__item--title">{item.Name}</h2>
-              <ul className={`accordion__item--list ${this.state.idOpen === +item.Id ? "visible" : "hidden"}`}>
-                  {item.children.map((object, index) =>
-                    <li className="detail" key={index}>
-                      <input type="checkbox" id={object.name} name={object.name} />
-                        <label htmlFor={object.name}>{object.name}</label>
-                    </li>
-                  )}
-              </ul>
-            </li>
+          {this.state.services.map((service, index) =>
+            <li className="accordion__list--item" id={index} key={index} onClick={this.handleAccordion}>
+              <h2 className="accordion__list--title">
+                {service.map(item => item.ServiceCategory.Name)[0]}
+              </h2>
+              <div className={`details ${this.state.idOpen === index ? "visible" : "hidden"}`}>
+                <div className="detail__list--free">
+                  <h3>Free</h3>
+                  <ul className="detail__list">
+                    {service.map((detail, index) =>
+                      <li className="detail__list--item" key={index}>
+                        <h4>{detail.Free ? detail.Name : null}</h4>
+                      </li>
+                    )}
+                  </ul>
+                </div>
+                <div className="detail__list--extra">
+                  <h3>Extra</h3>
+                  <ul className="detail__list">
+                    {service.map((detail, index) =>
+                      <li className="detail__list--item" key={index}>
+                        <h4>{!detail.Free ? detail.Name : null}</h4>
+                      </li>
+                    )}
+                  </ul>
+                </div>
+              </div>
+            </li>  
           )}
         </ul>
       </div>
