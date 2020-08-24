c848bd964a16f73eb7882a955db6417c9f145d33
veroena <veronica.enamorado@gmail.com>
2020-08-24 03:59:15 +0200

    last review and change readme

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -12,7 +12,7 @@ export const Accordion = (props) => {
                 {service.map(item => item.ServiceCategory.Name)[0]}
               </h2>
               <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={`accordion__list--chevron ${idOpen === index ? "open" : "close"}`}>
-              <path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z"/>
+                {idOpen === index ? <path d="M0 9h24v6h-24z"/> : <path d="M24 9h-9v-9h-6v9h-9v6h9v9h6v-9h9z"/>}
               </svg>
             </div>
             <Detail service={service} index={index} idOpen={idOpen} />
